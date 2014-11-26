64eb25848d60e233a6ac4cfe6fa1ca90d5c85656
xiaoehaier <xiaoehaier@163.com>
2014-11-26 16:21:29 +0800

    University of Washington, CSE 190 M Homework Assignment 6: ASCIImation

---
+++ myanimations.js
@@ -0,0 +1,10 @@
+ANIMATIONS["Custom"] = ANIMATIONS["custom"] = ANIMATIONS["CUSTOM"] = 
+"   \<\") \n"+
+"   ( \>\\ \n" +
+"    \" \\\\ \n" +
+"       \\  ejm \n" +
+"=====\n"+
+"   \>\") \n" +
+"   ( \>\\ \n" +
+"    \" \\\\ \n" +
+"       \\  ejm \n";
